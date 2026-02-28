e5f93fe778e6901b922ce77e9dac771b99d35e62
KARTHIK <122384867+KARTHIK522001@users.noreply.github.com>
2026-02-28 13:16:56 +0530

    Create scaling.sh

---
+++ scaling.sh
@@ -0,0 +1,7 @@
+kubectl apply -f nginx-deployment.yaml -n monitor
+kubectl get pods -n monitor
+kubectl describe deployment nginx-deployment -n monitor
+kubectl expose deployment nginx-deployment --type=LoadBalancer --port=80 --name=nginx-service -n monitor
+kubectl get svc -n monitor
+kubectl autoscale deployment nginx-deployment --cpu-percent=2 --min=2 --max=5 -n monitor
+kubectl get hpa -n monitor
